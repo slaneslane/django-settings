9930fcae0af9dfe188ee3960eda59d831377a959
Slane <slane@wp.pl>
2019-10-07 14:45:53 +0200

    [CHANGE] Further spliting of settings between base, test and production.

--- split/settings/environments/test.py
+++ split/settings/environments/test.py
@@ -1,3 +1,11 @@
 INSTALLED_APPS += [
     'tests.dummy_app',
 ]
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3'
+}
+
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+)
